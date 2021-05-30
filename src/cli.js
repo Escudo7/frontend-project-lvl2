bcd7460cafddc898bd87907825ee158392d3989b
Anton <anton@ya.ru>
2021-05-30 16:43:36 +0500

    add tests

--- src/cli.js
+++ src/cli.js
@@ -1,40 +1,9 @@
 import { Command } from 'commander/esm.mjs';
 import { readFileSync } from 'fs';
 import { extname } from 'path';
-import _ from 'lodash';
+import genDiff from './genDiff'
 
 const version = '0.0.1';
-
-const findDiff = (data1, data2) => {
-  const rowAddPrefix = '  + ';
-  const rowRemovePrefix = '  - ';
-  const rowSharePrefix = '    ';
-
-  const keys = _.union(Object.keys(data1), Object.keys(data2)).sort();
-  const diff = keys.reduce((acc, key) => {
-    if (!_.has(data1, key)) {
-      return [...acc, `${rowAddPrefix}${key}: ${data2[key]}`];
-    }
-
-    if (!_.has(data2, key)) {
-      return [...acc, `${rowRemovePrefix}${key}: ${data1[key]}`];
-    }
-
-    if (data1[key] === data2[key]) {
-      return [...acc, `${rowSharePrefix}${key}: ${data1[key]}`];
-    }
-
-    const row1 = `${rowRemovePrefix}${key}: ${data1[key]}`;
-    const row2 = `${rowAddPrefix}${key}: ${data2[key]}`;
-
-    return [...acc, row1, row2];
-  }, []);
-
-  diff.unshift('{');
-  diff.push('}');
-
-  return `\n${diff.join('\n')}\n`;
-};
 
 export default () => {
   const program = new Command();
@@ -55,8 +24,9 @@ export default () => {
 
       const contentParsed1 = JSON.parse(content1);
       const contentParsed2 = JSON.parse(content2);
+      const diff = genDiff(contentParsed1, contentParsed2);
 
-      console.log(findDiff(contentParsed1, contentParsed2));
+      console.log(diff);
     })
     .description('Compares two configuration files and shows a difference.')
     .parse(process.argv);
