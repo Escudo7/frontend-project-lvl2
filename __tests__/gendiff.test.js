f01c39216b0ddf59c866e9dfd447ab6579224f85
Anton <anton@ya.ru>
2021-05-30 18:16:41 +0500

    update tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,11 +1,17 @@
 import { test, expect } from '@jest/globals';
 import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 import genDiff from '../src/genDiff.js';
 
 test('genDiff', () => {
-  const content1 = JSON.parse(readFileSync('__fixtures__/json1.json'));
-  const content2 = JSON.parse(readFileSync('__fixtures__/json2.json'));
-  const result = readFileSync('__fixtures__/json_result', 'utf8');
+  const currentPath = fileURLToPath(import.meta.url);
+  const currentDir = dirname(currentPath);
+  const getPath = (fileName) => `${currentDir}/../__fixtures__/${fileName}`;
+
+  const content1 = JSON.parse(readFileSync(getPath('json1.json')));
+  const content2 = JSON.parse(readFileSync(getPath('json2.json')));
+  const result = readFileSync(getPath('json_result'), 'utf8');
 
   expect(genDiff(content1, content2)).toBe(result);
 });
