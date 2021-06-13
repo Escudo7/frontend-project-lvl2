f26ffcf4595f4f3b4553e00c0b413081ae4ba06f
Anton <anton@ya.ru>
2021-06-13 15:05:03 +0500

    refactoring

--- src/stylish.js
+++ src/stylish.js
@@ -21,54 +21,51 @@ const renderObject = (obj, depth) => {
   });
 };
 
-const render = (diffList, depth) => (
-  diffList.reduce((acc, item) => {
+const renderObjectDiff = (name, value, depth, rowPrefix) => {
+  const data = [];
+  data.push(`${rowSharePrefix.repeat(depth - 1)}${rowPrefix}${name}: {`);
+  data.push(renderObject(value, depth + 1));
+  data.push(`${rowSharePrefix.repeat(depth)}}`);
+
+  return data;
+};
+
+const renderKeyDiff = (name, value, depth, rowPrefix) => (
+  _.isObject(value)
+    ? renderObjectDiff(name, value, depth, rowPrefix)
+    : `${rowSharePrefix.repeat(depth - 1)}${rowPrefix}${name}: ${value}`
+);
+
+const renderer = (diffList, depth) => {
+  const renderItem = (item) => {
     const newData = [];
 
     if (_.has(item, 'children')) {
       newData.push(`${rowSharePrefix.repeat(depth)}${item.name}: {`);
-      newData.push(render(item.children, depth + 1));
+      newData.push(renderer(item.children, depth + 1));
       newData.push(`${rowSharePrefix.repeat(depth)}}`);
     }
 
     if (_.has(item, 'oldValue')) {
-      if (_.isObject(item.oldValue)) {
-        newData.push(`${rowSharePrefix.repeat(depth - 1)}${rowRemovePrefix}${item.name}: {`);
-        newData.push(renderObject(item.oldValue, depth + 1));
-        newData.push(`${rowSharePrefix.repeat(depth)}}`);
-      } else {
-        newData.push(`${rowSharePrefix.repeat(depth - 1)}${rowRemovePrefix}${item.name}: ${item.oldValue}`);
-      }
+      newData.push(renderKeyDiff(item.name, item.oldValue, depth, rowRemovePrefix));
     }
 
     if (_.has(item, 'newValue')) {
-      if (_.isObject(item.newValue)) {
-        newData.push(`${rowSharePrefix.repeat(depth - 1)}${rowAddPrefix}${item.name}: {`);
-        newData.push(renderObject(item.newValue, depth + 1));
-        newData.push(`${rowSharePrefix.repeat(depth)}}`);
-      } else {
-        newData.push(`${rowSharePrefix.repeat(depth - 1)}${rowAddPrefix}${item.name}: ${item.newValue}`);
-      }
+      newData.push(renderKeyDiff(item.name, item.newValue, depth, rowAddPrefix));
     }
 
     if (_.has(item, 'value')) {
-      if (_.isObject(item.newValue)) {
-        newData.push(`${rowSharePrefix.repeat(depth)}${item.name}: {`);
-        newData.push(renderObject(item.value, depth + 1));
-        newData.push(`${rowSharePrefix.repeat(depth)}}`);
-      } else {
-        newData.push(`${rowSharePrefix.repeat(depth)}${item.name}: ${item.value}`);
-      }
+      newData.push(renderKeyDiff(item.name, item.value, depth, rowSharePrefix));
     }
 
-    const newDataFlatted = newData.flat();
+    return newData;
+  };
 
-    return [...acc, ...newDataFlatted];
-  }, [])
-);
+  return _.flatMapDeep(diffList, renderItem);
+};
 
 export default (diff) => {
-  const rows = render(diff, 1);
+  const rows = renderer(diff, 1);
   rows.unshift('{');
   rows.push('}');
 
